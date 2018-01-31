f4a34c13759e94c12788f64f10140af87f45875b
Jiří Prokop <jprokop@synaptiko.cz>
2018-01-31 21:55:49 +0100

    Implement initial front-end & back-end

---
+++ index.mjs
@@ -0,0 +1,26 @@
+import http2 from 'http2'
+import fs from 'fs'
+import koa from 'koa'
+import koaStatic from 'koa-static'
+import koaMount from 'koa-mount'
+
+const options = {
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem')
+}
+const port = 8443
+const app = new koa()
+
+async function api(ctx, next) {
+  await next()
+  ctx.body = {
+		content: 'Hello, world! From API!'
+	}
+}
+
+app.use(koaStatic('public'))
+app.use(koaMount('/hyperhtml', koaStatic('node_modules/hyperhtml/esm')))
+app.use(koaMount('/api', api))
+
+const server = http2.createSecureServer(options, app.callback())
+server.listen(port, () => console.log(`Listening on port ${port}!`))
---
+++ public/bootstrap.js
@@ -0,0 +1,26 @@
+import hyperHTML from './hyperhtml/index.js'
+
+const render = hyperHTML(document.body)
+let header = 'Hello, world!'
+
+async function loadData() {
+	const res = await fetch('/api')
+	const body = await res.json()
+
+	header = body.content
+	update(render)
+}
+
+function update(render) {
+  render`
+    <div>
+      <h1>${header}</h1>
+      <h2>It is ${new Date().toLocaleTimeString()}.</h2>
+    </div>
+  `
+}
+
+loadData()
+
+setInterval(update, 1000, render)
+update(render)
